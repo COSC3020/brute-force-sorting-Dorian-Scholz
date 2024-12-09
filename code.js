60eb167c18cb8435b25a7a3eaf6061adc989502b
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-08 17:38:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,40 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(a)
+    {
+
+    function generatePermutations(arr)
+        {
+        if (arr.length <= 1) return [arr];
+        const permutations = [];
+        for (let i = 0; i < arr.length; i++) 
+            
+        {
+            const rest = arr.slice(0, i).concat(arr.slice(i + 1));
+            const restPermutations = generatePermutations(rest);
+            
+            for (const perm of restPermutations)
+                
+            {
+                permutations.push([arr[i]].concat(perm));
+            }
+        }
+        return permutations;
+    }
+
+    const permutations = generatePermutations(a);
+
+    for (const perm of permutations)
+        
+        {
+        if (perm.every((val, idx) => idx === 0 || perm[idx - 1] <= val))
+        {
+            
+            for (let i = 0; i < a.length; i++) 
+            {
+                a[i] = perm[i];
+            }
+            return permutations.length; 
+        }
+    }
+
+    return -1; 
 }
